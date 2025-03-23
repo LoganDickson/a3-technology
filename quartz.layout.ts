67166e1bcf355140f44a84414226a6108e6392e1
Logan Dickson <mr.leaux@proton.me>
2025-03-23 13:41:57 -0400

    Quartz sync: Mar 23, 2025, 1:41 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -8,8 +8,8 @@ export const sharedPageComponents: SharedLayout = {
   afterBody: [],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
+      //GitHub: "https://github.com/jackyzha0/quartz",
+      "Anderson School District Three": "https://www.acsd3.org",
     },
   }),
 }
